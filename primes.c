2655d8e022b5a6d70d6925af816e74e870baeea4
Jon Wedell <jonwedell@gmail.com>
2014-03-20 21:41:42 -0500

    Minor logic improvements and code cleanup.

--- primes.c
+++ primes.c
@@ -1,75 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 // Eponymous main method
 int main(int argc, char *argv[]){
 	
     // Variables that we will use
-    unsigned long biggest,psqrt,where,prime,numprimes,i,n,m;
+    unsigned long biggest,numprimes,i,m;
     
     // Variables that we won't use... just kidding!
-    where = 1;
-	prime = 0;
     numprimes = 1; // Don't forget #2!
     
     // Verify the user input and convert it to a long
-    if ((argc != 2) || (!sscanf (argv[1],"%lu",&biggest))){
+    if ((argc != 2) || (!sscanf (argv[1],"%lu",&biggest)) || (biggest < 2)){
         printf("You lose, you lose, you lose!\n");
         return 2;
     }
     
-    // We only need to store the primes up to sqrt(largest number to check) so long as we print each prime as we find them
-    unsigned long bigdiv = sqrt(biggest)+1;
-    static unsigned long *primes = NULL;
-    if (!primes) { primes = calloc(bigdiv/2, sizeof(long)); }
+    //printf("2\n");
     
     // We'll keep a char array of numbers that are worth checking
-    // As a minor optimization we won't bother checking numbers divisible by 2...
+    // and as a bonus use null as "could be prime" so we don't have
+    // to actually initialize the array
     static char *search = NULL;
-    if (!search) { search = calloc(biggest+1, sizeof(char)); }
-    for (i=1;i<biggest;i+=2){ search[i] = '1'; }
+    if (!search) { search = calloc(biggest, sizeof(char)); }
     
-	// Go through the possible primes...
-	for(i=3;i<biggest+1;i=i+2){
+	// Go through the possible primes (no need to look at multiples of 2)...
+	for(i=3;i<biggest;i=i+2){
         
         // Check if we already know if this number is not prime
-        if (search[i] == '1'){ 
-            prime = 1;
-            psqrt = sqrt(i);
-            
-            // Try dividing the number by all of our known primes
-            for (n=0;n<where;n++){
-                if ((primes[n] == 0) || (primes[n] > psqrt)){ break; }
-                if (i % primes[n] == 0){
-                    prime = 0;
-                    break;
-                }
-            }
-            
-            // If the number is prime...
-            if (prime){
-                // Print the number
-                //printf("%lu\n",i);
-                numprimes++;
-                // Store the number in our array of primes
-                if (where<=bigdiv){
-                    primes[where] = i;
-                    where++;
-                }
+        if (search[i] == '\0'){ 
+            // Either count the number or print it
+            numprimes++; //printf("%lu\n",i);
 
-                // Remove all numbers divisble by the new prime from the search pool
-                for (m=i;m<=(biggest);m = m + i){
-                    search[m] = '0';
-                }
-            }
+            // Remove all numbers divisble by the new prime from the search pool
+            for (m=i;m<biggest;m = m + i){ search[m] = '1'; }
         }
 	}
 
     printf("%lu primes found.\n",numprimes);
-
-	//free(primes);
-    //free(search);
+    free(search);
 
 	return 0;
 }
